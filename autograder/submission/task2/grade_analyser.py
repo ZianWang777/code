0089e25e3c574909a22a69859d8a524332aff42e
ZianWang777 <vbqf1090@leeds.ac.uk>
2024-11-02 22:16:01 +0000

    Update grade_analyser.py

--- autograder/submission/task2/grade_analyser.py
+++ autograder/submission/task2/grade_analyser.py
@@ -29,3 +29,40 @@ Your output files must be structured exactly as described - output files for all
 Note:
 Your code will only be tested on valid files in the format shown in the 4 example files in this folder - you do not need to validate any data.
 '''
+def classification(average_grade):
+    if average_grade>=70:
+        return "1"
+    elif average_grade>=60:
+        return "2:1"
+    elif average_grade>=50:
+        return "2:2"
+    elif average_grade>=40:
+        return "3"
+    else:
+        return "F"
+file_name=input()
+with open(file_name,'r') as infile:
+    reader=infile.readlines()[1:]
+out_file_name=f"{file_name}_out.csv"
+output=[]
+
+for line in reader:
+    row=line.split(',')
+    id=row[0]
+    grades=row[1:]
+    sum=0
+    number=0
+    for i in grades:
+        if not i=='' and not i=='\n':
+            sum+=int(i)
+            number+=1
+    average=sum/number
+    cl=classification(average)
+    output.append(f"{id},{f"{average:.2f}"},{cl}")
+with open(f"{file_name}_out.csv",'w') as outfile:
+    for i in output:
+        outfile.write(f"{i}\n")
+     
+
+
+
